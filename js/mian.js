71f3a49880fc67fb03509e4a8ba48d176d04df9b
Miaozi Yin <yin00030@algonquinlive.com>
2017-05-24 11:34:37 -0400

    Create mian.js

---
+++ js/mian.js
@@ -0,0 +1,19 @@
+/*global console*/
+var letter;
+var i = 0;
+var k = 0;
+
+while (!letter) {
+
+	letter = prompt("Enter a chacter?");
+	console.log(letter);
+
+}
+
+
+for (i = 0; i < 10; i += 1) {
+	for (k = 0; k <= i; k += 1) {
+		document.write(letter);
+	}
+	document.write("<br>");
+}
